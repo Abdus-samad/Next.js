f5c38ddd2f126bd0c69efe596c7126a400829dc5
ajaojnr5 <abdussamadajao@gmail.com>
2021-03-18 16:25:17 +0100

    Note a

---
+++ pages/api/index.js
@@ -0,0 +1,15 @@
+import nc  from 'next-connect'
+
+const handler = nc()
+.get((req, res) => {
+    res.json({message: 'ok'})
+})
+.post((req, res) => {
+    res.json({message: 'posted'})
+})
+
+
+
+
+
+export default handler
